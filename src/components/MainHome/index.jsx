dc9bebe4b987c9c9d83136b7081b6fab660ad4bd
Ant0yne <antoine.soliv@gmail.com>
2024-04-03 22:14:58 +0200

    mobile-layout-home-test

--- src/components/MainHome/index.jsx
+++ src/components/MainHome/index.jsx
@@ -5,16 +5,20 @@ import "./mainHome.scss";
 const MainHome = () => {
 	return (
 		<main>
-			<section id="home">
-				<div id="comic-home">
-					<Link to="/comics">
-						<button></button>
-					</Link>
-				</div>
-				<div id="cha-home">
-					<Link to="/characters"></Link>
-				</div>
-			</section>
+			<div className="container">
+				<section id="home">
+					<div id="comic-home">
+						<Link to="/comics">
+							<button></button>
+						</Link>
+					</div>
+					<div id="cha-home">
+						<Link to="/characters">
+							<button></button>
+						</Link>
+					</div>
+				</section>
+			</div>
 		</main>
 	);
 };
