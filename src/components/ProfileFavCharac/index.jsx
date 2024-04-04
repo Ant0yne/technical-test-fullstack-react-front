14ffe539c2ad711d338610f762d515d7826402f8
Ant0yne <antoine.soliv@gmail.com>
2024-04-04 22:52:01 +0200

    display-fav-liste-profile

--- src/components/ProfileFavCharac/index.jsx
+++ src/components/ProfileFavCharac/index.jsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
 import "./profileFavCharac.scss";
 
-const ProfileFavCharac = ({ favComics, setFavComics }) => {
-	return <div>ProfileFavCharac</div>;
+const ProfileFavCharac = ({ favCharacters, setFavCharacters }) => {
+	return (
+		<section id="profile-fav-comics">
+			<h3>Your favorite characters</h3>
+			{favCharacters.map((fav) => {
+				// The path if click on comic with the comic id
+				const link = "/comics/" + fav._id;
+				return (
+					<div key={fav._id}>
+						<Link to={link}>
+							<img
+								src={
+									fav.thumbnail.path +
+									"/portrait_small." +
+									fav.thumbnail.extension
+								}
+								alt=""
+							/>
+							{/* <p>{_id}</p>
+				<p>{title}</p>
+			<p>{description}</p> */}
+						</Link>
+					</div>
+				);
+			})}
+		</section>
+	);
 };
 
 export default ProfileFavCharac;
--- src/components/ProfileFavComics/index.jsx
+++ src/components/ProfileFavComics/index.jsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
 import "./profileFavComics.scss";
 
-const ProfileFavComics = () => {
-	return <div>ProfileFavComics</div>;
+const ProfileFavComics = ({ favComics, setFavComics }) => {
+	return (
+		<section id="profile-fav-comics">
+			<h3>Your favorite comics</h3>
+			{favComics.map((fav) => {
+				// The path if click on comic with the comic id
+				const link = "/comic/" + fav._id;
+				return (
+					<div key={fav._id}>
+						<Link to={link}>
+							<img
+								src={
+									fav.thumbnail.path +
+									"/portrait_small." +
+									fav.thumbnail.extension
+								}
+								alt=""
+							/>
+							{/* <p>{_id}</p>
+				<p>{title}</p>
+			<p>{description}</p> */}
+						</Link>
+					</div>
+				);
+			})}
+		</section>
+	);
 };
 
 export default ProfileFavComics;
