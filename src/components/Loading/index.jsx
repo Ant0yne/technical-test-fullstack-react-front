a9b44cbe1c47a234117de3ca6ae62b184e0bb5a7
Ant0yne <antoine.soliv@gmail.com>
2024-04-07 18:59:30 +0200

    Loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,7 +1,18 @@
+import Header from "../Header";
+import Footer from "../Footer";
+
 import "./loading.scss";
 
 const Loading = () => {
-	return <div>Loading</div>;
+	return (
+		<section id="loading">
+			<h2>
+				Chargement <span id="dot-1">.</span>
+				<span id="dot-2">.</span>
+				<span id="dot-3">.</span>
+			</h2>
+		</section>
+	);
 };
 
 export default Loading;
